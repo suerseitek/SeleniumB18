b625a6290e1abd1039f479f39993e9fb25c17bb3
Seitek Suerkulov <suerseitek@gmail.com>
2020-06-02 16:26:10 -0400

    1day selenium

---
+++ src/test/java/test/SeleniumTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    public static void main(String[] args) throws Exception {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+         Thread.sleep(2000);
+        driver.navigate().back();
+
+        Thread.sleep(2000);
+        driver.navigate().forward();
+
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+
+       Thread.sleep(2000);
+        driver.get("http://facebook.com");
+
+        System.out.println(driver.getTitle());
+
+        System.out.println(driver.getCurrentUrl());
+
+        driver.getPageSource();
+
+
+    }
+
+
+}
